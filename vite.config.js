0a4d481d28b40ae38918c0b922b3371a44bda1cb
salman <mrsalmanxzs@gmail.com>
2025-07-31 21:13:50 +0530

    ddd

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [tailwindcss(), react()],
+  server: {
+    watch: {
+      ignored: ['**/db.json'] // ✅ Glob pattern to match db.json in any folder
+    }
+  }
 })
